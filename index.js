96392b3385d18a8e31d36368f1ca333a15433ae0
AJBababa <alvarojbustos@gmail.com>
2025-09-30 16:51:02 +0200

    login completado

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.get('/', (req, res) => {
   res.render('index', { title: 'Hola mundo con EJS', param: req.query.param })
 })
 
-/*IsAuth = (req, res, next) => {
-  if (req.cookies && req.cookies.username) {
-    return next();
-  }
-  res.redirect('/login');
-}*/
+
 
 IsAdmin = (req, res, next) => {
   if (req.cookies && req.cookies.username && req.cookies.role === 'admin') {
@@ -36,19 +32,16 @@ IsUser = (req, res, next) => {
   res.redirect('/login');
 }
 
-
-
-/*app.get('/home', IsAuth, (req, res) => {
-  res.render('home', { title: 'Home', param: req.query.param });
-});*/
-
-
 app.get('/admin', IsAdmin, (req, res) => {
   res.render('admin', { title: 'Admin', param: req.query.param });
 });
 app.get('/user', IsUser, (req, res) => {
   res.render('user', { title: 'User', param: req.query.param });
 });
+
+
+
+
 
 //login
 app.get('/login', (req, res) => {
@@ -61,7 +54,7 @@ app.get('/logout', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  
+
   const { username, password } = req.body;
   console.log(req.body);
 
